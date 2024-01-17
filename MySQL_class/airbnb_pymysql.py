b5a777464c6acd9992b7c37422267384c3edc364
Samiiz <skboss159@gmail.com>
2024-01-17 10:09:51 +0900

    airbnb question1

---
+++ MySQL_class/airbnb_pymysql.py
@@ -0,0 +1,19 @@
+import pymysql
+
+connection = pymysql.connect(
+    host='localhost', 
+    user='root', 
+    password='samiiz', 
+    db='airbnb',
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+with connection.cursor() as cursor:
+    # 문제 1 : 새로운 제품 추가
+    sql = "INSERT INTO Products (productName, price, stockQuantity) VALUES (%s, %s, %s)"
+    cursor.execute(sql, ('Python Book', 10000, 10))
+    connection.commit()
+
+
+cursor.close()
