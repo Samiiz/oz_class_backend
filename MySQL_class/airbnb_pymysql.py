b8c75731184ad4010a03ab8f116ac6f1bc54284d
Samiiz <skboss159@gmail.com>
2024-01-17 10:10:15 +0900

    airbnb question2

--- MySQL_class/airbnb_pymysql.py
+++ MySQL_class/airbnb_pymysql.py
@@ -10,10 +10,15 @@ connection = pymysql.connect(
 )
 
 with connection.cursor() as cursor:
-    # 문제 1 : 새로운 제품 추가
-    sql = "INSERT INTO Products (productName, price, stockQuantity) VALUES (%s, %s, %s)"
-    cursor.execute(sql, ('Python Book', 10000, 10))
-    connection.commit()
+    # # 문제 1 : 새로운 제품 추가
+    # sql = "INSERT INTO Products (productName, price, stockQuantity) VALUES (%s, %s, %s)"
+    # cursor.execute(sql, ('Python Book', 10000, 10))
+    # connection.commit()
+
+    # 문제 2 : 고객 목록(제품 정보) 조회
+    cursor.execute("SELECT * FROM Products")
+    for book in cursor.fetchall():
+        print(book)
 
 
 cursor.close()
