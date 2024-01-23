a75b0092659c543e104efa3c5f48a6770c96eb83
Samiiz <skboss159@gmail.com>
2024-01-23 17:37:03 +0900

    make a sample

--- bootstrap/kream_crawl.py
+++ bootstrap/kream_crawl.py
@@ -47,10 +47,25 @@ browser = webdriver.Chrome(service=service, options=options)
 
 # 진행 과정
 # for문은 두가지로 나눈다 1. 전체상품 / 2. 신규상품
+# 아 근데 중복되네................................
+# 아니지, 전체상품 보여줄때만 중복제거 해서 보여주면 되잖아
+# male, female, kidz 로 나뉘고 all_products 보여줄때는 중복 제거해서 보여주면 되겠다
+# 그러면 pk 빼고 성별 / 카테고리 / 이너카테고리 별로 긁어와서 okeu dokey
+
+
+# for 성별
+#   category_gender = 해당성별
+#   for 상위 카테고리
+#       category_products = 해당 카테고리
+#       for 하위 카테고리
+#           category_inner = 해당 하위 카테고리
+#           객체 하나씩 들어가서
+#           product_name, brand, model_number ,price_released ,price_last ,representative_color ,directly_buy ,directly_sell 추출
+#           하고나서 spl = insert into ...
+#           근데 이게 효율이 좋을까...
 # 
 # 
-# 
-# 
+
 
 
 
@@ -61,7 +76,7 @@ browser = webdriver.Chrome(service=service, options=options)
 # category_inner = category_products에 따라서 for문으로 진행
 # product_name = .main-title-container > .title
 # brand = .left-container > .title-text 안나오면 .subtitle
-# [PK]model_number = .detail-box[2] > .product_info
+# model_number = .detail-box[2] > .product_info
 # price_released = detail-price > .price_info[1]
 # price_last = .detail-box[1] > .product_info
 # representative_color = .color-target
