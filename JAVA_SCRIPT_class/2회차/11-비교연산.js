81d2eb148b041cfa8e22404522c7170a28ca29c3
Samiiz <skboss159@gmail.com>
2024-02-14 17:28:28 +0900

    Lets learn django

--- JAVA_SCRIPT_class/2회차/11-비교연산.js
+++
@@ -1,6 +0,0 @@
-// let age = parseInt(prompt("당신의 나이는?"))
-// console.log(20 <= age)
-
-let name = prompt("당신의 이름은?")
-document.write(`사용자의 이름은 ${name}입니다.<br>`)
-document.write(`우리가 찾던 사람인가? ${"박상훈" === name}`)
