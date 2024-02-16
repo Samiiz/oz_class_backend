e68b70d33ec398fe1b0d09b710463a4292402862
Samiiz <skboss159@gmail.com>
2024-02-16 10:55:39 +0900

    modified the path to use rest api

--- oz-django/config/urls.py
+++ oz-django/config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feeds/', include('feeds.urls')),
-    path('questions/', include('questions.urls')),
+    path('api/v1/feeds/', include('feeds.urls')),
+    path('api/v1/questions/', include('questions.urls')),
+    path('api/v1/users/', include('users.urls')),
+    # path('api/v1/reviews/', include('reviews.urls')),
 ]
